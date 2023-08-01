1bb6971ad10773881266cefd4420691cd5a9bd70
walkerspencer <136180719+walkerspencer@users.noreply.github.com>
2023-08-01 15:52:53 -0300

    Update Exerc3.java

--- Exerc3.java
+++ Exerc3.java
@@ -1,25 +1,23 @@
-package aula;
+package atividades;
 
 import java.util.Scanner;
 
-public class Exerc3 {
+public class Aula3Exercicio3 {
 
 	public static void main(String[] args) {
-
-		Scanner letra = new Scanner (System.in);		
-		System.out.println("Insira a letra F ou M para obter o resultado ");
-					String sexo = letra.nextLine();
-						
-			if (sexo.equals("F")) {
-				System.out.println("Feminino");				
-			}else if (sexo.equals("M")) {
-				System.out.println("Masculino");
-				}else{
-					System.out.println("Sexo invalido")
-				};
-				}
-			
 		
+		Scanner letra = new Scanner (System.in);
+		System.out.println("Insira a letra F (Feminino) ou M (Masculino) ");
+				String sexo = letra.nextLine();
+				
+				if (sexo.equals("F")) {
+					System.out.println("Feminino");
+				}else if (sexo.equals("M")){
+					System.out.println("Masculino");
+						
+					} else {
+						System.out.println("Sexo Invalido");
+					}
+				}
 	}
 
-}
